864b7efbe8ebeb685d5d0c458badc88c4e463199
Nickel <114811360+N1ckell@users.noreply.github.com>
2025-03-08 12:16:39 +0000

    Add files via upload
    partially implemented kruskals algorithm (doesn't work with different orders of pairs of vertices)
    questions can be marked, though there is considered only one correct answer
    marking updates the graph and displays vertices in a corresponding colour (red green yellow etc.)

---
+++ menus/quiz_func/custom_graph.py
@@ -0,0 +1,35 @@
+import menus.quiz_func.graph_objects as gobj
+import random as r
+
+MAX_WEIGHT = 1500
+
+def getRandomGraph1():
+
+    vertices = [gobj.Vertex([50,50],'a'),
+                gobj.Vertex([50,300],'b'),
+                gobj.Vertex([200,450],'c'),
+                gobj.Vertex([600,100],'d'),
+                gobj.Vertex([450,600],'e'),
+                gobj.Vertex([400,300],'f'),
+                gobj.Vertex([500,25],'g'),
+                gobj.Vertex([200,50],'h'),
+            ]
+
+    edges = [gobj.Edge(vertices[0], vertices[1], label = r.randint(1,MAX_WEIGHT)),
+            gobj.Edge(vertices[0], vertices[2], label = r.randint(1,MAX_WEIGHT)),
+            gobj.Edge(vertices[2], vertices[4], label = r.randint(1,MAX_WEIGHT)),
+            gobj.Edge(vertices[4], vertices[3], label = r.randint(1,MAX_WEIGHT)),
+            gobj.Edge(vertices[0], vertices[7], label = r.randint(1,MAX_WEIGHT)),
+            gobj.Edge(vertices[3], vertices[6], label = r.randint(1,MAX_WEIGHT)),
+            gobj.Edge(vertices[5], vertices[2], label = r.randint(1,MAX_WEIGHT)),
+
+            gobj.Edge(vertices[3], vertices[7], label = r.randint(1,MAX_WEIGHT)),
+            gobj.Edge(vertices[4], vertices[7], label = r.randint(1,MAX_WEIGHT)),
+            gobj.Edge(vertices[5], vertices[7], label = r.randint(1,MAX_WEIGHT))
+    ]
+
+    return gobj.Graph(vertices,edges)
+
+#g = getRandomGraph1()
+#g.kruskalsAlgorithm()
+
---
+++ menus/screen_transitions.py
@@ -0,0 +1,27 @@
+import app_functions as af
+import ttkbootstrap as ttk
+import menus.main_menu as mmenu
+import menus.marking_menu as markm
+import menus.quiz_creation as qc
+import menus.quiz_func.custom_quiz as cq
+import menus.quiz_menu as qm
+import menus.quiz_func.quiz_objects as qobj
+
+def gotoQuizCreation(app : ttk.Window):
+    af.clearWindow(app)
+    qc.drawQuizCreator(app)
+
+def gotoQuiz(app : ttk.Window, combobox : ttk.Combobox):
+    af.clearWindow(app)
+    quiz = cq.generateQuiz2(int(combobox.get()))
+    #print(isinstance(quiz.questions[quiz.current_question], qobj.KruskalsQuestion))
+    qm.initQuiz(app, quiz)
+
+
+def gotoMarkingMenu(app : ttk.Window, quiz : qobj.Quiz):
+    af.clearWindow(app)
+    markm.drawMarkingMenu(app, quiz)
+
+def gotoMainMenu(app : ttk.Window):
+    af.clearWindow(app)
+    mmenu.drawMenu(app)
